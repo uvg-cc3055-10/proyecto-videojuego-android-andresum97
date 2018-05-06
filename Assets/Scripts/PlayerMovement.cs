4e6c84f662caa9c3029133cf291a9842952b4e40
andresum97 <andresum1309@gmail.com>
2018-05-05 18:26:30 -0600

    Implementación de botones

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class PlayerMovement : MonoBehaviour {
+
+    float dirX, dirY;
+    // Use this for initialization
+
+    [Range(1f, 20f)]
+    public float moveSpeed = 5f;
+    void Start () {
+		
+	}
+
+    // Update is called once per frame
+    void Update() {
+        dirX = CrossPlatformInputManager.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
+        dirY = CrossPlatformInputManager.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
+        transform.position = new Vector2(transform.position.x + dirX, transform.position.y + dirY);
+
+    }
+}
